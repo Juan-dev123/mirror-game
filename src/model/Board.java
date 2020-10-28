4e331e01f48bbfa282cac70098b3fef1d51c49c9
Juanpis <378tech@gmail.com>
2020-10-28 15:29:08 -0500

    RF1 con pruebas unitarias

--- src/model/Board.java
+++ src/model/Board.java
@@ -35,52 +35,54 @@ public class Board {
 	
 	private void createBoard(Integer column, Grid last) {
 		if(columns==1) {
-			createRow(1, firstGrid);
+			createColumn(1, firstGrid);
 		}else if(column<columns) {
 			if(column==1) {
-				Grid last1 = createRow(1, firstGrid);
+				Grid last1 = createColumn(rows, firstGrid);
 				Grid grid = createGrid();
-				
-				Grid last2 = createRow(1, grid);
+				Grid last2 = createColumn(rows, grid);
 				linkRows(rows, last1, last2);
-				createBoard(column++, last2);
+				createBoard(++column, last2);
 			}else {
 				Grid grid = createGrid();
-				Grid last2 = createRow(1, grid);
+				Grid last2 = createColumn(rows, grid);
 				linkRows(rows, last, last2);
-				createBoard(column++, last2);
+				createBoard(++column, last2);
 			}
 		}
 	}
 	
-	private Grid createRow(Integer row, Grid last) {
-		if(row>0) {
+	private Grid createColumn(Integer row, Grid last) {
+		if(row==1) {
+			return last;
+		}else {
 			Grid nextGrid = createGrid();
 			last.setDown(nextGrid);
-			createRow(row--, nextGrid);
+			nextGrid.setUp(last);
+			return createColumn(--row, nextGrid);
 		}
-		return last;
+		
 	} 
 	
 	private Grid getBeginningOfRow(Integer row, Grid first) {
-		if(row!=1) {
-			getBeginningOfRow(row--, first.getDown());
+		if(row>1) {
+			getBeginningOfRow(--row, first.getDown());
 		}
 		return first;
 	}
 	
 	private void linkRows(Integer row, Grid last1, Grid last2) {
-		if(row<=rows && last2!=null) {
+		if(row>=1) {
 			last1.setRight(last2);
 			last2.setLeft(last1);
-			linkRows(row++, last1.getUp(), last2.getUp());
+			linkRows(--row, last1.getUp(), last2.getUp());
 		}
 	}
 	
 	private Grid createGrid() {
 		Grid grid;
 		if(mirrorsAdded<mirrors) {
-			int typeOfMirror = (int)Math.floor(Math.random()*(4));
+			int typeOfMirror = (int)Math.floor(Math.random()*(3));
 			grid=new Grid(typeOfMirror);
 			if(typeOfMirror!=0) {
 				mirrorsAdded++;
@@ -94,15 +96,54 @@ public class Board {
 	private void addMirrors(Grid next, int row, int column) {
 		if((row<=rows && column<=columns) || mirrorsAdded<mirrors) {
 			if(column==columns) {
-				addMirrors(getBeginningOfRow(row--, firstGrid), row--, 1);
+				addMirrors(getBeginningOfRow(--row, firstGrid), --row, 1);
 			}else if(next.getTypeMirror()==0) {
 				int typeOfMirror = (int)Math.floor(Math.random()*(2)+1);
 				next.setTypeMirror(typeOfMirror);
 				mirrorsAdded++;
-				addMirrors(next.getRight(), row, column++);
+				addMirrors(next.getRight(), row, ++column);
 			}
 		}
 	}
-	
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getMirrors() {
+		return mirrors;
+	}
+
+	public void setMirrors(int mirrors) {
+		this.mirrors = mirrors;
+	}
+
+	public int getMirrorsAdded() {
+		return mirrorsAdded;
+	}
+
+	public void setMirrorsAdded(int mirrorsAdded) {
+		this.mirrorsAdded = mirrorsAdded;
+	}
+
+	public Grid getFirstGrid() {
+		return firstGrid;
+	}
+
+	public void setFirstGrid(Grid firstGrid) {
+		this.firstGrid = firstGrid;
+	}
 	
 }
--- test/model/BoardTest.java
+++ test/model/BoardTest.java
@@ -1,5 +1,71 @@
 package model;
 
-public class BoardTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.InvalidNumberException;
+import exceptions.NegativeNumberException;
+
+class BoardTest {
 	
+	private Board game;
+	public void setupScenary() throws InvalidNumberException, NegativeNumberException {
+		game = new Board(3, 4, 8);
+	}
+	@Test
+	void test() {
+		try {
+			setupScenary();
+			assertNotNull(game.getFirstGrid());
+			assertNull(game.getFirstGrid().getUp());
+			assertNull(game.getFirstGrid().getLeft());
+			assertNotNull(game.getFirstGrid().getDown());
+			assertNull(game.getFirstGrid().getDown().getLeft());
+			assertNotNull(game.getFirstGrid().getDown().getDown());
+			assertNull(game.getFirstGrid().getDown().getDown().getLeft());
+			assertNotNull(game.getFirstGrid().getDown().getDown().getDown());
+			assertNull(game.getFirstGrid().getDown().getDown().getDown().getLeft());
+			assertNull(game.getFirstGrid().getDown().getDown().getDown().getDown());
+			
+			Grid first2 = game.getFirstGrid().getRight();
+			assertNotNull(first2);
+			assertNull(first2.getUp());
+			assertNotNull(first2.getRight());
+			assertNotNull(first2.getLeft());
+			assertNotNull(first2.getDown());
+			assertNotNull(first2.getDown().getLeft());
+			assertNotNull(first2.getDown().getRight());
+			assertNotNull(first2.getDown().getDown());
+			assertNotNull(first2.getDown().getDown().getRight());
+			assertNotNull(first2.getDown().getDown().getLeft());
+			assertNotNull(first2.getDown().getDown().getDown());
+			assertNotNull(first2.getDown().getDown().getDown().getLeft());
+			assertNotNull(first2.getDown().getDown().getDown().getRight());
+			assertNull(first2.getDown().getDown().getDown().getDown());
+			
+			Grid first3 = first2.getRight();
+			assertNotNull(first3);
+			assertNull(first3.getUp());
+			assertNull(first3.getRight());
+			assertNotNull(first3.getLeft());
+			assertNull(first3.getRight());
+			assertNotNull(first3.getDown());
+			assertNotNull(first3.getDown().getLeft());
+			assertNull(first3.getDown().getRight());
+			assertNotNull(first3.getDown().getDown());
+			assertNotNull(first3.getDown().getDown().getLeft());
+			assertNull(first3.getDown().getDown().getRight());
+			assertNotNull(first3.getDown().getDown().getDown());
+			assertNotNull(first3.getDown().getDown().getDown().getLeft());
+			assertNull(first3.getDown().getDown().getDown().getRight());
+			assertNull(first3.getDown().getDown().getDown().getDown());
+			
+		}catch(InvalidNumberException in) {
+			fail("Todos los numeros son validos");
+		}catch(NegativeNumberException ne) {
+			fail("El numero no es negativo");
+		}
+	}
+
 }
